28ca185655d17fd36c942cde12da3df5deb8e660
Guillermo Natali Ulla <22guillermonatali@gmail.com>
2023-05-25 22:31:44 -0300

    commit 12

--- app/app.py
+++ app/app.py
@@ -2,8 +2,8 @@ from flask import (
     Blueprint,flash,g,redirect,render_template,request,url_for
     )
 from werkzeug.exceptions import abort
-from todo.auth import login_required
-from todo.db import get_db
+from app.auth import login_required
+from app.db import get_db
 
 bp = Blueprint('todo',__name__)
 
